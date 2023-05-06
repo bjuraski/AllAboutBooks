f2368d7c353c0b3e47860f97332731202dbb9097
Borna Juraski <borna.juraski@outlook.com>
2023-05-06 16:18:30 +0200

    Add project files.

---
+++ AllAboutBooksWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AllAboutBooksWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
