fe8d2b06b252cfd2656673ae63cf59f8e1198c94
Borna Juraski <borna.juraski@outlook.com>
2023-05-07 16:30:24 +0200

    added Category Controller and Seed Data in migration

---
+++ AllAboutBooksWeb/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using AllAboutBooksWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllAboutBooksWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public CategoryController(ApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var categories = await _applicationDbContext
+            .Categories
+            .OrderBy(c => c.DisplayOrder)
+            .ToListAsync();
+
+        return View(categories);
+    }
+}
---
+++ AllAboutBooksWeb/Migrations/20230507135024_SeedCategoryTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AllAboutBooksWeb.Migrations;
+
+/// <inheritdoc />
+public partial class SeedCategoryTable : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.InsertData(
+            table: "Categories",
+            columns: new[] { "Id", "DisplayOrder", "Name" },
+            values: new object[,]
+            {
+                { 1L, 1L, "Action" },
+                { 2L, 2L, "SciFi" },
+                { 3L, 3L, "History" }
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DeleteData(
+            table: "Categories",
+            keyColumn: "Id",
+            keyValue: 1L);
+
+        migrationBuilder.DeleteData(
+            table: "Categories",
+            keyColumn: "Id",
+            keyValue: 2L);
+
+        migrationBuilder.DeleteData(
+            table: "Categories",
+            keyColumn: "Id",
+            keyValue: 3L);
+    }
+}
