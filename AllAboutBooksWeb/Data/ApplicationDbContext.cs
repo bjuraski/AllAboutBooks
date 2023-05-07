dc74980377b0edcded02b5d4d043de695bfa3999
Borna Juraski <borna.juraski@outlook.com>
2023-05-07 15:37:32 +0200

    EF setup and Category model added and table with migration

---
+++ AllAboutBooksWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AllAboutBooksWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllAboutBooksWeb.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
---
+++ AllAboutBooksWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AllAboutBooksWeb.Models;
+
+public class Category
+{
+    [Key]
+    public long Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; } = string.Empty;
+
+    public long DisplayOrder { get; set; }
+}
