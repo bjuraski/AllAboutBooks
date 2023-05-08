316fb3c957380c49f4418d5f4644c6e53e3f73dc
Borna Juraski <borna.juraski@outlook.com>
2023-05-08 18:40:07 +0200

    Added project to dsemonstrate Categories in Razor Pages

---
+++ AllAboutBooksWebRazor_Temp/Pages/BootstrapIcons.cs
@@ -0,0 +1,9 @@
+﻿namespace AllAboutBooksWebRazor_Temp.Pages;
+
+public static class BootstrapIcons
+{
+    public const string PlusSquare = "bi bi-plus-square";
+    public const string PencilSquare = "bi bi-pencil-square";
+    public const string XSquare = "bi bi-x-square";
+    public const string Trash = "bi bi-trash";
+}
