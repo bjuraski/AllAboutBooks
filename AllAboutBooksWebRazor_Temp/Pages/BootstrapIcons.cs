d243190b4cb1d0c5bf7397b56dfcca2b07c0e128
Borna Juraski <borna.juraski@outlook.com>
2023-05-08 20:20:06 +0200

    remove sample project and visually tweaked views

--- AllAboutBooksWebRazor_Temp/Pages/BootstrapIcons.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AllAboutBooksWebRazor_Temp.Pages;
-
-public static class BootstrapIcons
-{
-    public const string PlusSquare = "bi bi-plus-square";
-    public const string PencilSquare = "bi bi-pencil-square";
-    public const string XSquare = "bi bi-x-square";
-    public const string Trash = "bi bi-trash";
-}
